883eb88c04aa553c409c741a85af12ec87bf3eb9
Neema_Mwende <gracepete12@gmail.com>
2023-08-01 17:29:22 +0300

    js to make the cards responsive

---
+++ Red_Road.js
@@ -0,0 +1,21 @@
+<!-- Add this script tag just before the closing body tag -->
+ document.addEventListener('DOMContentLoaded', function () {
+    const circularCard = document.getElementById('circularCard');
+    // Add hover effect to the card
+    circularCard.addEventListener('mouseenter', function () {
+      circularCard.classList.add('bg-success');
+      circularCard.classList.remove('bg-success');
+    });
+
+    circularCard.addEventListener('mouseleave', function () {
+      circularCard.classList.remove('bg-success');
+      circularCard.classList.add('bg-success');
+    });
+
+    // Add click effect to the card
+    circularCard.addEventListener('click', function () {
+      // Replace this with the desired action when the card is clicked, e.g., redirect to a new page or perform an action.
+      alert('You clicked on Circular Economy!');
+    });
+  });
+
